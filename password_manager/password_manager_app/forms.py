d91122f3a8557cff6b6df5aa27be4d3f75e36b72
Aytekin <aytexinet35@hotmail.com>
2023-08-21 23:43:55 +0300

    login-register tasarlandı.

--- password_manager/password_manager_app/forms.py
+++ password_manager/password_manager_app/forms.py
@@ -15,3 +15,6 @@ class PasswordForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(max_length=100, widget=forms.PasswordInput)
     confirm_password = forms.CharField(max_length=100, widget=forms.PasswordInput)
+
+
+
